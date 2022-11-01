117200521a68dc3752d5e7f473932765158c7902
renhoders <lorenzoavv@gmail.com>
2022-11-01 14:08:46 -0300

    Fix Script Selenium
    CAmbio de forma de Traducir las peliculas.

--- script_se.py
+++ script_se.py
@@ -12,25 +12,18 @@ import time
 driver_location = '/usr/bin/chromedriver'
 binary_location = '/usr/bin/google-chrome'
 
+#traducimos el texto todos las clases .review
+#iniciamos googletrans , sudo pipe3 install googletrans 4.0.0rc1
+translator = googletrans.Translator()
+
 options = webdriver.ChromeOptions()
 options.binary_location = binary_location
 
 driver = webdriver.Chrome(executable_path=driver_location,options=options)
 
+#mi contexto local
 driver.get("http://127.0.0.1:8080/")
 
-#traducimos el texto todos las clases .review
-#iniciamos googletrans , sudo pipe3 install googletrans 4.0.0rc1
-translator = googletrans.Translator()
-
-#obtenemos todos los componentes que cumplan con .review
-reviews = driver.find_elements(By.XPATH, "//p[@class='review']")
-
-#recorremos el arreglo reviews para poder traducir de Ingles a Espaniol
-for review in reviews:
-    driver.execute_script("arguments[0].textContent = arguments[1];", review, translator.translate(review.text,dest='es').text)
-
-time.sleep(5)
 
 #agregamos las 2 peliculas.
 driver.find_element(By.ID, "add-movie").click()
@@ -49,7 +42,16 @@ driver.find_element(By.NAME, "rating").send_keys("5")
 driver.find_element(By.NAME,"review").send_keys("En la mañana del 6 de junio de 1944, comienzo de la invasión de Normandía, los soldados estadounidenses se preparan para desembarcar en la playa de Omaha.")
 driver.find_element(By.CSS_SELECTOR, ".btn").click()
 
-time.sleep(5)
+nums = [1, 2] #identidicadores de las peliculas para traducir
+#contatenamos la id de las peliculas y traducimos su review
+#finalmente guardamos.
+for n in nums:
+    url = "/edit/"+str(n)
+    driver.find_element(By.XPATH,'//a[@href="'+url+'"]').click()
+    txtAreaReview = driver.find_element(By.NAME, "review")
+    txtAreaReview.clear()
+    txtAreaReview.send_keys(translator.translate(txtAreaReview.text,dest='es').text)
+    driver.find_element(By.CSS_SELECTOR, ".btn").click()
 
 driver.quit()
 
